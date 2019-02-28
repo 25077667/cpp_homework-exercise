ece3d07a7891d9e0e40a93c8325904f52a0ac5fc
25077667 <zxc25077667@gmail.com>
2019-02-28 23:04:02 +0800

    rename

--- homework/HW1/ch2/p4.cpp
+++ homework/HW1/ch2/p4.cpp
@@ -2,24 +2,6 @@
 using namespace std;
 int main()
 {
-    map<char, int> table;
-    table['S'] = 10;
-    table['A'] = 9;
-    table['B'] = 8;
-    table['C'] = 7;
-    table['D'] = 6;
-    table['E'] = 5;
-    table['F'] = 0;
-    int subjects;
-    double sum=0;
-    cout<<"Please input the number of subjects";
-    cin>>subjects;
-    for(int i=0;i<subjects;i++){
-        char tmp;
-        cout<<"Please input the "<<i+1<<" subject grade(S A B C D E F):";
-        cin>>tmp;
-        sum+=table[tmp];
-    }
-    cout<<sum/subjects<<endl;
+    cout<<"3 5 7"<<endl;
     return 0;
 }
