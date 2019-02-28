68b0d241f58e00a696bb9a7077e16f97379faa3a
25077667 <zxc25077667@gmail.com>
2019-03-01 02:27:47 +0800

    fix bugs

--- homework/HW1/ch2/p2.cpp
+++ homework/HW1/ch2/p2.cpp
@@ -1,8 +1,7 @@
-#include <iostream>
 #include <cmath>
+#include <iostream>
 using namespace std;
-int main()
-{
+int main() {
     int initial_deposit, fixed_amount_withdrawn;
     double interest_rate;
     cout << "input format \"initial_deposit interest_rate fixed_amount_withdrawn\" in a line\n";
@@ -10,13 +9,12 @@ int main()
     int counter = 0;
     if ((initial_deposit - fixed_amount_withdrawn) * interest_rate > fixed_amount_withdrawn)
         cout << "The interest earned every month is greater than the fixed withdrawal amount." << endl;
-    else
-    {
+    else {
         for (counter = 0; initial_deposit > 0; counter++)
-            initial_deposit = ((initial_deposit - fixed_amount_withdrawn) * interest_rate / 100) + (initial_deposit - fixed_amount_withdrawn);
+            initial_deposit = ((initial_deposit - fixed_amount_withdrawn) * interest_rate) + (initial_deposit - fixed_amount_withdrawn);
         cout << counter << endl;
         //want to use recurrence equation solution to answer it in O(1), but have bugs.
-        //cout << 2 + (log((fixed_amount_withdrawn * (-1)) / (initial_deposit * interest_rate - fixed_amount_withdrawn * (initial_deposit + 1) * (initial_deposit + 1))) / log(interest_rate+1))<<endl;
+        //cout << ceil(1 + (log((fixed_amount_withdrawn * (1 + interest_rate)) / (fixed_amount_withdrawn * (1 + interest_rate) - initial_deposit * interest_rate)) / log(interest_rate + 1))) << endl;
     }
     return 0;
 }
