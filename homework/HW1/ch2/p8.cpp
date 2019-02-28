a0a106f452dfa2daf9c77c4a80910611eb216ea4
25077667 <zxc25077667@gmail.com>
2019-02-28 23:19:10 +0800

    p8 done

---
+++ homework/HW1/ch2/p8.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int celsius, fahrenheit=0;
+    for (celsius = 100; celsius != fahrenheit; celsius-- && (fahrenheit = celsius * 9 / 5 + 32));
+    cout << celsius << endl;
+    return 0;
+}
