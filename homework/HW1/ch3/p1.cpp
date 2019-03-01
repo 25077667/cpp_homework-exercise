195c22367820c40f10491ea5feacdae18aaeb6b6
25077667 <zxc25077667@gmail.com>
2019-03-01 18:10:16 +0800

    p1 need a function

--- homework/HW1/ch3/p1.cpp
+++ homework/HW1/ch3/p1.cpp
@@ -1,9 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int kilobits_per_megabyte = 8000;
+double video_size(double video_duration) {
+    return video_duration * 60 * 1000 / kilobits_per_megabyte;
+}
 int main() {
     double video_duration;  //in minutes
     cout << "Please input the video duration in minurtes" << endl;
     cin >> video_duration;
-    cout << video_duration * 60 * 1000 / kilobits_per_megabyte << endl;
+    cout << video_size(video_duration) << " megabytes." << endl;
 }
