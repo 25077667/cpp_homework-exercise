5ea8c0b1e5fe8843233be93314bc77fe63a98c86
25077667 <zxc25077667@gmail.com>
2019-03-06 00:45:45 +0800

    fix p6

--- homework/HW1/ch1/p6.cpp
+++ homework/HW1/ch1/p6.cpp
@@ -4,6 +4,11 @@ int main() {
     int miles, time;
     cout << "input the \"miles time\" in a line\n";
     cin >> miles >> time;
-    cout << 2 + ((miles - 2 < 0) ? 0 : (miles - 2) * 0.5) + ((miles - 6 < 0) ? 0 : (miles - 6) * 0.5) + (0.2) * time << endl;
+    if (miles <= 2)
+        cout << 2 + time * 0.2 << endl;
+    else if (miles <= 8)
+        cout << 2 + (miles - 2) * 0.5 + ((miles - 6 > 0) ? (miles - 6) : 0) + time * 0.2 << endl;
+    else
+        cout << 2 + 3 + (miles - 6) + time * 0.2 << endl;
     return 0;
 }
