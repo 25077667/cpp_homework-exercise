d496a4724c9699016d426efec126a9a81c438690
25077667 <zxc25077667@gmail.com>
2019-03-07 19:10:01 +0800

    bad problem

---
+++ homework/HW2/ch5/p8.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int get_random_number() {
+    random_device rd;
+    mt19937_64 mt_rand(rd());
+    return (mt_rand() % 365);
+}
+int main() {
+    int arr[365] = {0};
+    for (int i = 2; i < 51; i++) {
+        double sum = 0;
+        for (int k = 0; k < 5000; k++) {
+            for (int j = 0; j < i; j++)
+                arr[get_random_number()]++;
+
+            for (int j = 0; j < 365; j++)
+                if (arr[j] > 1){
+                    sum++;
+                    break;
+                }
+            memset(arr, 0, sizeof(arr));
+        }
+        //cout << endl;
+        cout << i << " " << sum/5000 << endl;
+    }
+    return 0;
+}
