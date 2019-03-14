dca2741b569fcbfcc230114b65799d6f79cfdbb4
25077667 <zxc25077667@gmail.com>
2019-03-14 10:34:03 +0800

    p10

---
+++ homework/HW3/p10.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Weight {
+    double kg, p, o;
+    void show() {
+        cout << "> In Kilograms " << kg << endl;
+        cout << "> In Pounds " << p << endl;
+        cout << "> In Ounces " << o << endl
+             << endl;
+    }
+
+   public:
+    void setWeightPounds() {
+        p = 110;
+        kg = p / 2.21;
+        o = p * 16;
+        show();
+    }
+    void setWeightKilograms() {
+        kg = 60;
+        p = kg * 2.21;
+        o = p * 16;
+        show();
+    }
+    void setWeightOunces() {
+        o = 1800;
+        p = 1800 / 16;
+        kg = p / 2.21;
+        show();
+    }
+};
+int main() {
+    Weight w;
+    cout << "set Kilograms 60" << endl;
+    w.setWeightKilograms();
+    cout << "set Pounds 110" << endl;
+    w.setWeightPounds();
+    cout << "set Ounces 1800" << endl;
+    w.setWeightOunces();
+    return 0;
+}
