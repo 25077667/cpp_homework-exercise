96d20f9435d77fec8adcb81f0b5f9da8a6322d97
25077667 <zxc25077667@gmail.com>
2019-03-18 23:59:40 +0800

    use vector to find prime

--- homework/HW1/ch2/p4.cpp
+++ homework/HW1/ch2/p4.cpp
@@ -4,19 +4,17 @@ int main() {
     // TA correction this problem
     vector<int> prime;
     prime.push_back(2);
-    unsigned int t = time(NULL);
-    for (int i = 3; i < INT_MAX; i += 2) {
+    for (int i = 3; i < 100; i += 2) {
         vector<int>::iterator iter;
         for (iter = prime.begin(); iter != prime.end(); iter++) {
-            if (i % (*iter) == 0 || (*iter) > sqrt(i))
+            if (i % (*iter) == 0 && (*iter) > sqrt(i)+1 )
                 break;
         }
         if (iter == prime.end())
             prime.push_back(i);
     }
-    cout << time(NULL) - t << endl;
-    //for (vector<int>::iterator iter = prime.begin(); iter != prime.end(); iter++)
-    //    cout << *iter << " ";
-    //cout << endl;
+    for (vector<int>::iterator iter = prime.begin(); iter != prime.end(); iter++)
+        cout << *iter << " ";
+    cout << endl;
     return 0;
 }
