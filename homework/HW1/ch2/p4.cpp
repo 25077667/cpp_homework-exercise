053b6d2b3c8042aa6a2d0efb1c19b89c335fcd7c
25077667 <zxc25077667@gmail.com>
2019-03-18 23:54:02 +0800

    run though 2147483647 need 147 seconds

--- homework/HW1/ch2/p4.cpp
+++ homework/HW1/ch2/p4.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 int main() {
     // TA correction this problem
-    cout << "3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97" << endl;
+    vector<int> prime;
+    prime.push_back(2);
+    unsigned int t = time(NULL);
+    for (int i = 3; i < INT_MAX; i += 2) {
+        vector<int>::iterator iter;
+        for (iter = prime.begin(); iter != prime.end(); iter++) {
+            if (i % (*iter) == 0 || (*iter) > sqrt(i))
+                break;
+        }
+        if (iter == prime.end())
+            prime.push_back(i);
+    }
+    cout << time(NULL) - t << endl;
+    //for (vector<int>::iterator iter = prime.begin(); iter != prime.end(); iter++)
+    //    cout << *iter << " ";
+    //cout << endl;
     return 0;
 }
