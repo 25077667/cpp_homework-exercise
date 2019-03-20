c032068442a84325ef8d2a3cdd7767c2f80ca5cd
25077667 <zxc25077667@gmail.com>
2019-03-20 18:18:53 +0800

    fix p8

--- homework/HW2/ch4/p8.cpp
+++ homework/HW2/ch4/p8.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 void output(int pounds, double ounces) {
-<<<<<<< HEAD
     cout << "it is " << pounds << " pounds and " << ounces << " ounces." << endl;
 }
 void calculating(double kg, double g) {
@@ -9,15 +8,6 @@ void calculating(double kg, double g) {
     double weight = kg * 1000 + g;  //g
     int pounds = (weight / 1000) * 2.2046;
     output(pounds, (weight - pounds / 2.2046 * 1000) / 1000 * 2.2046 * 16);
-=======
-    cout << "it is " << pounds << "pounds and " << ounces << " ounces." << endl;
-}
-void calculating(double kg, double g) {
-    //1 pound = 453.59237g
-    double weight = kg * 1000 + g;//g
-    int pounds = weight / 453.59237;
-    output(pounds, (weight - pounds * 453.59237) * 0.0352739619);
->>>>>>> d92da8f847b089888a8cdd1f4853d09fe2a92a66
 }
 void input() {
     double kg, g;
@@ -26,10 +16,6 @@ void input() {
     cout << "input g ";
     cin >> g;
     calculating(kg, g);
-<<<<<<< HEAD
-    //cout<<kg<<" "<<g<<endl;
-=======
->>>>>>> d92da8f847b089888a8cdd1f4853d09fe2a92a66
 }
 
 int main() {
