b78ba40907913ab3fa71a60fbfff8da018ef2132
25077667 <zxc25077667@gmail.com>
2019-03-28 01:45:07 +0800

    p14 debug time error input

--- homework/HW2/ch4/p14.cpp
+++ homework/HW2/ch4/p14.cpp
@@ -1,31 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-void convert_time(string start_time, string end_time, int& start, int& ending) {
-    stringstream ss;
-    ss.str("");
-    ss.clear();
-    ss << start_time;
-    ss >> start;
-    ss.str("");
-    ss.clear();
-    ss << end_time;
-    ss >> ending;
-}
 int main() {
+    int start, ending;
     string start_time, end_time;
     while (cout << "input start time && end time(**** ****)" && cin >> start_time >> end_time) {
-        if (start_time.length() != 4 || end_time.length() != 4) {
+        start = ((start_time[0] - '0') * 10 + (start_time[1] - '0')) * 60 + (start_time[2] - '0') * 10 + (start_time[3] - '0');
+        ending = ((end_time[0] - '0') * 10 + (end_time[1] - '0')) * 60 + (end_time[2] - '0') * 10 + (end_time[3] - '0');
+        if (start_time.length() != 4 || end_time.length() != 4 || start > 1440 || ending > 1440 || (start_time[0] - '0') * 10 + (start_time[1] - '0') > 24 || (end_time[0] - '0') * 10 + (end_time[1] - '0') > 24 || (start_time[2] - '0') * 10 + (start_time[3] - '0') > 59 || (end_time[2] - '0') * 10 + (end_time[3] - '0') > 59) {
             cout << "input error" << endl;
             continue;
         }
-        int start, ending;
-        convert_time(start_time, end_time, start, ending);
-        struct tm t1 = {0};
-        struct tm t2 = {0};
-        t1.tm_hour = start / 100;
-        t1.tm_min = start % 100;
-        t2.tm_hour = ending / 100;
-        t2.tm_min = ending % 100;
+        struct tm t1 = {0}, t2 = {0};
+        t1.tm_min = start;
+        t2.tm_min = ending;
         int result = (difftime(mktime(&t2), mktime(&t1))) / 60;
         cout << "it is " << ((result > 0) ? result : result + 1440) << " minutes" << endl;
     }
