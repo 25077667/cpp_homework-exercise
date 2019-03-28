99dfffa99375607636b990eb54fcc237fbaba448
25077667 <zxc25077667@gmail.com>
2019-03-28 23:06:42 +0800

    p3 boundry condition

--- exercise/0328/p3.cpp
+++ exercise/0328/p3.cpp
@@ -4,9 +4,9 @@ using namespace std;
 void bubble(vector<int> &arr, int n) {
     for (int i = n - 1; i >= 0; i--) {
         for (int j = 0; j < i; j++)
-            if (arr[j] > arr[j + 1]) {
-                int tmp = arr[j];
-                arr[j] = arr[j + 1];
+            if (arr.at(j) > arr[j + 1]) {
+                int tmp = arr.at(j);
+                arr.at(j) = arr[j + 1];
                 arr[j + 1] = tmp;
             }
     }
@@ -15,33 +15,30 @@ void selection(vector<int> &arr, int n) {
     for (int i = n - 1; i >= 0; i--) {
         int index = i;
         for (int j = 0; j < i; j++) {
-            if (arr[j] > arr[index])
+            if (arr.at(j) > arr[index])
                 index = j;
         }
-        int tmp = arr[i];
-        arr[i] = arr[index];
+        int tmp = arr.at(i);
+        arr.at(i) = arr[index];
         arr[index] = tmp;
     }
 }
 
 int main() {
     int choice;
-    while(cin>>choice){
+    while (cin >> choice) {
         vector<int> data;
         int tmp;
-        cout<<"enter numbers";
+        cout << "enter numbers";
         while (cin >> tmp && tmp != -1)
             data.push_back(tmp);
-        if(choice ==1){
-            bubble(data,data.size());
-        }
-        else{
+        if (choice == 1)
+            bubble(data, data.size());
+        else
             selection(data, data.size());
-        }
-        for(vector<int>::iterator iter=data.begin(); iter!= data.end();iter++){
-            cout<<*iter<<" ";
-        }
-        cout<<endl;
+        for (vector<int>::iterator iter = data.begin(); iter != data.end(); iter++)
+            cout << *iter << " ";
+        cout << endl;
     }
     return 0;
 }
