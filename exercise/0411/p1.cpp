c484b4f5acb62aefff00b0603d467140a3d3e6a6
25077667 <zxc25077667@gmail.com>
2019-04-11 15:44:06 +0800

    readable for code

--- exercise/0411/p1.cpp
+++ exercise/0411/p1.cpp
@@ -1,28 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+const int STUDENT_NUMBERS = 5;
 struct Grade {
-    int score[5] = {0};
+    int score[STUDENT_NUMBERS] = {0};
     bool isCompelete = false;
 };
 
 int main() {
     Grade students;
+    //init for teacher
     cout << "init score:";
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < STUDENT_NUMBERS; i++)
         cout << students.score[i] << " ";
     cout << endl
-         << "Finish intime " << students.isCompelete << endl;
-    cout << "please input score";
+         << "Finish intime " << students.isCompelete << endl
+         << "please input score";
+
+    //start
     students.isCompelete = true;
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < STUDENT_NUMBERS; i++) {
         cin >> students.score[i];
         if (students.score[i] <= 0)
             students.isCompelete = false;
     }
-    for (int i = 0; i < 5; i++)
+
+    for (int i = 0; i < STUDENT_NUMBERS; i++)
         cout << students.score[i] << ",";
-    cout <<endl<<students.isCompelete <<endl;
+    cout << endl
+         << students.isCompelete << endl;
 
     return 0;
 }
