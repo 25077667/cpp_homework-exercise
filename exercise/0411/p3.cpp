a5690127828c26584e60c563a22ffb228ddbde62
25077667 <zxc25077667@gmail.com>
2019-04-11 15:36:09 +0800

    enhance p3

--- exercise/0411/p3.cpp
+++ exercise/0411/p3.cpp
@@ -1,35 +1,21 @@
 #include <iostream>
-#include <cmath>
-using namespace std;
-
-struct Point{
-    int x,y;
+struct Point {
+    int x, y;
 };
-
-struct Line{
-    Point first,second;
-    int vx,vy;
+struct Line {
+    Point first, second;
+    int vx, vy;
 };
-
 int main() {
-    Line a,b;
-    char trash;
-    cin >> trash;
-    cin >> a.first.x >> a.first.y;
-    cin >> trash;
-    cin >> a.second.x >> a.second.y;
-    cin >> trash;
-    cin >> b.first.x >> b.first.y;
-    cin >> trash;
-    cin >> b.second.x >> b.second.y;
-    a.vx = a.second.x - a.first.x;
-    a.vy = a.second.y - a.first.y;
-    b.vx = b.second.x - b.first.x;
-    b.vy = b.second.y - b.first.y;
-    if(a.vx *b.vx +a.vy*b.vy == 0)
-        cout<<"vertical"<<endl;
-    else
-        cout<<"Not Vertical"<<endl;
-    
+    Line l[2];
+    for (int i = 0; i < 2; i++) {
+        std::cout << char('A' + i * 2) << std::endl;
+        std::cin >> l[i].first.x >> l[i].first.y;
+        std::cout << char('B' + i * 2) << std::endl;
+        std::cin >> l[i].second.x >> l[i].second.y;
+        l[i].vx = l[i].second.x - l[i].first.x;
+        l[i].vy = l[i].second.y - l[i].first.y;
+    }
+    std::cout << ((l[0].vx * l[1].vx + l[0].vy * l[1].vy) ? "Not Vertical" : "Vertical") << std::endl;
     return 0;
 }
