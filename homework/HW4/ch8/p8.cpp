55cbb799ce1e64c6f56a8051f38332b9c07dec53
25077667 <zxc25077667@gmail.com>
2019-04-13 23:43:11 +0800

    TA request is strange

--- homework/HW4/ch8/p8.cpp
+++ homework/HW4/ch8/p8.cpp
@@ -6,23 +6,19 @@ class Temperature {
     ~Temperature(){};
     void setF(double value);
     friend inline const bool operator==(const Temperature& a, const Temperature& b);
+    friend istream& operator>>(istream& input, Temperature& thisTemperature);
     friend ostream& operator<<(ostream& output, const Temperature& thisTemperature);
 
    private:
     double k, c, f;
+    int i;
 };
 int main() {
     Temperature t1, t2;
-    double value1, value2;
-    cout << "Please input a temperature1 in Fahrenheit.";
-    cin >> value1;
-    t1.setF(value1);
+    cin >> t1;
     cout << t1 << endl;
-    cout << "Please input a temperature2 in Fahrenheit.";
-    cin >> value2;
-    t2.setF(value2);
+    cin >> t2;
     cout << t2 << endl;
-
     cout << "temperature1 == temperature2 : " << ((t1 == t2) ? "true" : "false") << endl;
     return 0;
 }
@@ -33,6 +29,12 @@ void Temperature::setF(double value) {
 }
 const bool operator==(const Temperature& a, const Temperature& b) {
     return a.k == b.k;
+}
+istream& operator>>(istream& input, Temperature& thisTemperature) {
+    cout << "Please input a temperature in Fahrenheit.";
+    input >> thisTemperature.f;
+    thisTemperature.setF(thisTemperature.f);
+    return input;
 }
 ostream& operator<<(ostream& output, const Temperature& thisTemperature) {
     output << "In Kelvin: " << thisTemperature.k << endl
