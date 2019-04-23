b70adbae3c10c77142dbd615ec0d495710da247d
25077667 <zxc25077667@gmail.com>
2019-04-23 13:36:51 +0800

    p1 done

--- homework/HW3/ch7/p1.cpp
+++ homework/HW3/ch7/p1.cpp
@@ -2,78 +2,101 @@
 using namespace std;
 
 class RainbowColor {
-    map<char, int> table;
+    int color;
+
    public:
-    RainbowColor();
+    RainbowColor(int);
+    RainbowColor(char);
     void testColor(int);
     void testColor(char);
-    bool getColorByName();
+    int getColorByName();
     bool nextColorNum();
 };
 int main() {
-    RainbowColor rainbow;
+    RainbowColor rainbowA(1), rainbowB('R');
     cout << "testing constructor RainbowColor(char)" << endl;
-    rainbow.testColor('R');
-    rainbow.testColor('O');
-    rainbow.testColor('Y');
-    rainbow.testColor('G');
-    rainbow.testColor('B');
-    rainbow.testColor('I');
-    rainbow.testColor('P');
+
+    rainbowB.testColor('R');
+    rainbowB.testColor('O');
+    rainbowB.testColor('Y');
+    rainbowB.testColor('G');
+    rainbowB.testColor('B');
+    rainbowB.testColor('I');
+    rainbowB.testColor('P');
 
     cout << "testing constructor RainbowColor(int)" << endl;
     for (int i = 1; i < 8; i++)
-        rainbow.testColor(i);
-    while (cout << "get the rainbow by name and output rainbow color\n" && rainbow.getColorByName()) {};
-    while (cout << "testing next rainbow color\n" && rainbow.nextColorNum()) {};
+        rainbowA.testColor(i);
+    while (cout << "get the rainbow by name and output rainbow color\n" && rainbowA.getColorByName())
+        ;
+    while (cout << "testing next rainbow color\n" && rainbowB.nextColorNum())
+        ;
 }
-RainbowColor::RainbowColor() {
-    table['R'] = 1;
-    table['O'] = 2;
-    table['Y'] = 3;
-    table['G'] = 4;
-    table['B'] = 5;
-    table['I'] = 6;
-    table['P'] = 7;
+
+RainbowColor::RainbowColor(int value) {
+    color = value;
+}
+RainbowColor::RainbowColor(char c) {
+    int value;
+    if (c == 'R')
+        value = 1;
+    else if (c == 'O')
+        value = 2;
+    else if (c == 'Y')
+        value = 3;
+    else if (c == 'G')
+        value = 4;
+    else if (c == 'B')
+        value = 5;
+    else if (c == 'I')
+        value = 6;
+    else
+        value = 7;
+    color = value;
 }
 void RainbowColor::testColor(int num) {
-    map<char, int>::iterator iter;
-    for (iter = table.begin(); iter->second != num; iter++);
-    cout << iter->first << "\t" << iter->second << endl;
+    char colorTable[7] = {'R', 'O', 'Y', 'G', 'B', 'I', 'P'};
+    cout << num << "\t" << colorTable[num - 1] << endl;
 }
 void RainbowColor::testColor(char c) {
-    cout << c << "\t" << table[c] << endl;
+    int value;
+    if (c == 'R')
+        value = 1;
+    else if (c == 'O')
+        value = 2;
+    else if (c == 'Y')
+        value = 3;
+    else if (c == 'G')
+        value = 4;
+    else if (c == 'B')
+        value = 5;
+    else if (c == 'I')
+        value = 6;
+    else
+        value = 7;
+    cout << value << "\t" << c << endl;
 }
-bool RainbowColor::getColorByName() {
-    char c;
-    cin >> c;
-    map<char, int>::iterator iter;
-    iter = RainbowColor::table.find(c);
-    if (iter != table.end()) {
-        cout << iter->second << "\t" << iter->first << endl;
-        return true;
-    } else {
-        cout << "Not found" << endl;
-        return false;
+int RainbowColor::getColorByName() {
+    char colorTable[7] = {'R', 'O', 'Y', 'G', 'B', 'I', 'P'};
+    char inputChar;
+    cin >> inputChar;
+    char* p = find(colorTable, colorTable + 7, inputChar);
+    if (p == colorTable + 7) {
+        cout << "Not found!\nending loop" << endl;
+        return 0;
     }
+    int index = p - colorTable + 1;
+    cout << "current " << index << "\t" << inputChar << endl;
+    return index;
 }
 bool RainbowColor::nextColorNum() {
-    char c;
-    cin >> c;
-    map<char, int>::iterator iter;
-    iter = table.find(c);
-    if (iter != table.end()) {
-        cout << "current " << iter->first << "\t" << iter->second << endl;
-        for (iter = table.begin(); iter->second != table[c]+1 && iter!=table.end(); iter++);
-
-        if (iter->second)
-            cout << "next " << iter->first << "\t" << iter->second << endl;
-        else
-            cout << "next " << "R\t1" << endl;
-
-        return true;
-    } else {
-        cout << "Not Found" << endl;
-        return false;
-    }
+    char colorTable[7] = {'R', 'O', 'Y', 'G', 'B', 'I', 'P'};
+    int index = getColorByName();
+    if (index == 7)
+        cout << "next 1\tR" << endl;
+    else if (index == 0)
+        return 0;
+    else
+        cout << "next " << index+1 << "\t" << colorTable[index] << endl;
+    return true;
 }
