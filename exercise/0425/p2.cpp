19c47d458500996ecdb6a3de5cea0725455074e7
25077667 <zxc25077667@gmail.com>
2019-04-25 13:45:24 +0800

    p2 done

---
+++ exercise/0425/p2.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Record {
+   private:
+    string name, birthday;
+
+   public:
+    static int counter;
+    Record(string, string);
+    ~Record();
+    const string getName() { return name; }
+    const string getBirthday() { return birthday; }
+    int getCounter() { return counter; }
+};
+
+Record::Record(string n, string b) {
+    name = n;
+    birthday = b;
+    counter++;
+}
+
+Record::~Record() {
+}
+
+int Record::counter = 0;
+
+int main() {
+    vector<Record> students;
+    string n, b;
+    while (cout << "name? " && cin >> n && n != "exit") {
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "birthday? ";
+        getline(cin, b);
+        Record newRecord(n, b);
+        students.push_back(newRecord);
+    }
+    for (vector<Record>::iterator iter = students.begin(); iter != students.end(); iter++)
+        cout << iter->getName() << endl
+             << iter->getBirthday() << endl;
+
+    cout << "Total " << students.at(0).getCounter() << endl;
+    return 0;
+}
--- exercise/0425/p4.cpp
+++ exercise/0425/p4.cpp
@@ -1,6 +1,45 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <vector>
 using namespace std;
-
 int main() {
+    int final[11] = {0};
+    vector<int> grade;
+    vector<int>::iterator p;
+    int cache;
+    while (true) {
+        cin >> cache;
+        if (cache == -1)
+            break;
+        grade.push_back(cache);
+    }
+    for (p = grade.begin(); p != grade.end(); p++) {
+        if (*p >= 0 && *p < 10)
+            final[0]++;
+        else if (*p >= 10 && *p < 20)
+            final[1]++;
+        else if (*p >= 20 && *p < 30)
+            final[2]++;
+        else if (*p >= 30 && *p < 40)
+            final[3]++;
+        else if (*p >= 40 && *p < 50)
+            final[4]++;
+        else if (*p >= 50 && *p < 60)
+            final[5]++;
+        else if (*p >= 60 && *p < 70)
+            final[6]++;
+        else if (*p >= 70 && *p < 80)
+            final[7]++;
+        else if (*p >= 80 && *p < 90)
+            final[8]++;
+        else if (*p >= 90 && *p < 100)
+            final[9]++;
+        else
+            final[10]++;
+    }
+    for (int i = 0; i < 10; i++)
+        cout << final[i] << " grade(s) of [" << i * 10 << "," << (i + 1) * 10 - 1 << "]" << endl;
+    if (final[10] != 0)
+        cout << final[10] << " grade(s) of 100" << endl;
     return 0;
 }
