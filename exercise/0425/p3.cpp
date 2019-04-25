fd1dba1aa060231b2afcaf64d37521147b4a8b7c
25077667 <zxc25077667@gmail.com>
2019-04-25 14:19:19 +0800

    p3 done

--- exercise/0425/p3.cpp
+++ exercise/0425/p3.cpp
@@ -1,6 +1,73 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
+class date {
+   private:
+    string d;
+    int m;
 
+   public:
+    void add() {
+        cin >> d >> m;
+    }
+    void print() {
+        cout << d << " " << m << endl;
+    }
+    string getd() {
+        return d;
+    }
+    int getm() {
+        return m;
+    }
+};
+
+void search(vector<date> &x) {
+    vector<date>::iterator p;
+    string a;
+    cout << "input: ";
+    cin >> a;
+    for (p = x.begin(); p != x.end(); p++) {
+        if (p->getd() == a) {
+            cout << p->getm() << endl;
+            return;
+        }
+    }
+    cout << "no found" << endl;
+}
 int main() {
-    return 0;
+    date cache;
+    vector<date> data;
+    vector<date>::iterator p;
+    bool r = true;
+    while (r) {
+        int flag;
+        cout << "1.add data" << endl;
+        cout << "2.print" << endl;
+        cout << "3.search" << endl;
+        cout << "4.exit" << endl;
+        cin >> flag;
+        int g;
+        switch (flag) {
+            case 1:
+                cache.add();
+                data.push_back(cache);
+                break;
+            case 2:
+                g = 0;
+                for (p = data.begin(); p != data.end(); p++) {
+                    p->print();
+                    g += p->getm();
+                }
+                cout << "sum = " << g << endl;
+                break;
+            case 3:
+                search(data);
+                break;
+            case 4:
+                r = false;
+                break;
+            default:
+                break;
+        }
+    }
 }
