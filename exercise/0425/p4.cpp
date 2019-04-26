7c2a3cd4db121df51575a3f17b15aa49076ea0b9
25077667 <zxc25077667@gmail.com>
2019-04-27 01:13:44 +0800

    p4 done

--- exercise/0425/p4.cpp
+++ exercise/0425/p4.cpp
@@ -5,37 +5,11 @@ using namespace std;
 int main() {
     int final[11] = {0};
     vector<int> grade;
-    vector<int>::iterator p;
     int cache;
-    while (true) {
-        cin >> cache;
-        if (cache == -1)
-            break;
+    while (cin >> cache && cache != -1) 
         grade.push_back(cache);
-    }
-    for (p = grade.begin(); p != grade.end(); p++) {
-        if (*p >= 0 && *p < 10)
-            final[0]++;
-        else if (*p >= 10 && *p < 20)
-            final[1]++;
-        else if (*p >= 20 && *p < 30)
-            final[2]++;
-        else if (*p >= 30 && *p < 40)
-            final[3]++;
-        else if (*p >= 40 && *p < 50)
-            final[4]++;
-        else if (*p >= 50 && *p < 60)
-            final[5]++;
-        else if (*p >= 60 && *p < 70)
-            final[6]++;
-        else if (*p >= 70 && *p < 80)
-            final[7]++;
-        else if (*p >= 80 && *p < 90)
-            final[8]++;
-        else if (*p >= 90 && *p < 100)
-            final[9]++;
-        else
-            final[10]++;
+    for (vector<int>::iterator p = grade.begin(); p != grade.end(); p++) {
+        final[(*p / 10)]++;
     }
     for (int i = 0; i < 10; i++)
         cout << final[i] << " grade(s) of [" << i * 10 << "," << (i + 1) * 10 - 1 << "]" << endl;
