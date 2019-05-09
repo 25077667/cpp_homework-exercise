7a2a0c2b5d9aedfab6bd2b3433f07320a09e256d
25077667 <zxc25077667@gmail.com>
2019-05-09 13:40:24 +0800

    p3 done

---
+++ exercise/0509/p3.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string s;
+    cout << " 0 or - is exit." << endl;
+
+    while (cin.peek() == '0' || cin.peek() == '-')
+        exit(0);
+    cin >> s;
+    for (string::iterator iter = s.end() - 1; iter >= s.begin(); iter--)
+        cout << *iter;
+    main();
+}
