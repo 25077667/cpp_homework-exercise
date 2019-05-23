b2a5453a3236937f8c3fd999463f32636688e72e
25077667 <zxc25077667@gmail.com>
2019-05-23 13:37:03 +0800

    p2 done

--- exercise/0523/p2.cpp
+++ exercise/0523/p2.cpp
@@ -1,5 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main() {
+    int row, column;
+    cin >> row >> column;
+    int classicArray[row][column];
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            cin >> classicArray[i][j];
+
+    int** DynamicArray = new int*[row];
+
+    for (int i = 0; i < row; i++)
+        DynamicArray[i] = new int[column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            DynamicArray[i][j] = classicArray[i][j];
+
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++)
+            cout << classicArray[i][j] << "@" << &classicArray[i][j] << '\t';
+        cout << endl; 
+    }
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++)
+            cout << DynamicArray[i][j] << "@" << &DynamicArray[i][j] << '\t';
+        cout << endl;
+    }
     return 0;
 }
