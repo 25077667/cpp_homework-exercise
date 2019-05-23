a8cfdc140c8b43e7e26544ccf5408d8b825c277a
25077667 <zxc25077667@gmail.com>
2019-05-23 13:57:35 +0800

    p4 done

--- exercise/0523/p4.cpp
+++ exercise/0523/p4.cpp
@@ -1,5 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main() {
+    queue<double> q1, q2;
+    int selection;
+    while (cin >> selection && selection != 3) {
+        if (selection == 1) {
+            int tmp;
+            cin >> tmp;
+            q1.push(tmp);
+        } else if (selection == 2 && !q1.empty()) {
+            q1.pop();
+        } else {
+            cout << "Don't do that!" << endl;
+            exit(1);
+        }
+        while (!q1.empty()) {
+            cout << q1.front() << " ";
+            q2.push(q1.front());
+            q1.pop();
+        }
+        while (!q2.empty()) {
+            q1.push(q2.front());
+            q2.pop();
+        }
+        cout << endl;
+    }
+
     return 0;
 }
