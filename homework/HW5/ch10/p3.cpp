e6d2e3edcb25b9bf825691a7f9df685f53bb772d
25077667 <zxc25077667@gmail.com>
2019-05-27 17:30:18 +0800

    tail was \0, now is the last char

--- homework/HW5/ch10/p3.cpp
+++ homework/HW5/ch10/p3.cpp
@@ -7,8 +7,8 @@ int main() {
     int counter = 1;
     cout << "Enter a string less then 80 char." << endl;
     cin.getline(s, 80);
-    char *head = s-1, *tail = s + strlen(s);
-    for (; head < tail;) {
+    char *head = s, *tail = s + strlen(s) - 1;
+    while (head < tail) {
         counter += (isspace(*tail) ? 1 : 0) + (isspace(*head) ? 1 : 0);
         swap(*head, *tail);
         head++;
