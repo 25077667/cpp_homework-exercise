1258e7128313625d2247a6fe9103d38d72a887a5
25077667 <zxc25077667@gmail.com>
2019-06-13 16:13:45 +0800

    p4 done

---
+++ exercise/0613/p4.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include "pokemon.h"
+
+int main() {
+    Water_Pokemon water[3] = {Water_Pokemon("Squirtle", "water", 3, 10), Water_Pokemon("Wartortle", "water", 7, 20), Water_Pokemon("Blastoise", "water", 15, 40)};
+    Wood_Pokemon wood[3] = {Wood_Pokemon("Bulbasaur", "wood", 3, 10), Wood_Pokemon("Ivysaur", "wood", 7, 20), Wood_Pokemon("Venusaur", "wood", 15, 40)};
+    Fire_Pokemon fire[3] = {Fire_Pokemon("Charmander", "fire", 3, 10), Fire_Pokemon("Charmeleon", "fire", 7, 20), Fire_Pokemon("Charizard", "fire", 15, 40)};
+
+    vector<Pokemon*> allPokemon;
+    for (int i = 0; i < 3; i++) {
+        allPokemon.push_back(&(water[i]));
+        allPokemon.push_back(&(wood[i]));
+        allPokemon.push_back(&(fire[i]));
+    }
+    for (int i = 0; i < 9; i++)
+        allPokemon[i]->showDetail();
+
+    int index1, index2;
+    cin >> index1 >> index2;
+    battle(*(allPokemon.at(index1)), *(allPokemon.at(index2)));
+    return 0;
+}
