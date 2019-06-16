9400f2c82c9451efacada4240a692f94bc71e662
25077667 <zxc25077667@gmail.com>
2019-06-17 05:59:18 +0800

    typesetting code

--- homework/HW1/ch2/p7.cpp
+++ homework/HW1/ch2/p7.cpp
@@ -1,7 +1,6 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main()
-{
+int main() {
     map<char, int> table;
     table['S'] = 10;
     table['A'] = 9;
@@ -11,15 +10,15 @@ int main()
     table['E'] = 5;
     table['F'] = 0;
     int subjects;
-    double sum=0;
-    cout<<"Please input the number of subjects";
-    cin>>subjects;
-    for(int i=0;i<subjects;i++){
+    double sum = 0;
+    cout << "Please input the number of subjects";
+    cin >> subjects;
+    for (int i = 0; i < subjects; i++) {
         char tmp;
-        cout<<"Please input the "<<i+1<<" subject grade(S A B C D E F):";
-        cin>>tmp;
-        sum+=table[tmp];
+        cout << "Please input the " << i + 1 << " subject grade(S A B C D E F): ";
+        cin >> tmp;
+        sum += table[tmp];
     }
-    cout<<sum/subjects<<endl;
+    cout << sum / subjects << endl;
     return 0;
 }
