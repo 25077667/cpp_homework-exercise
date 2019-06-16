7a6847b81fb309805b5550b77360ab22e1ff55e8
25077667 <zxc25077667@gmail.com>
2019-06-17 06:12:59 +0800

    clean code

--- homework/HW1/ch3/p5.cpp
+++ homework/HW1/ch3/p5.cpp
@@ -1,22 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-class info {
+class Info {
    public:
+    Info() : height(0), weight(0), age(0) {}
     double hat_size() { return weight / height * 2.9; };
     double Jacket_size() { return height * weight / 288 + ((age > 30) ? (double)((int)((age - 30) / 10) * 0.125) : 0); };
     double waist() { return weight / 5.7 + ((age > 28) ? (double)((int)((age - 28) / 2) * 0.1) : 0); };
-    bool input_height() {
-        cin >> height;
-        return true;
-    };
-    bool input_weight() {
-        cin >> weight;
-        return true;
-    };
-    bool input_age() {
-        cin >> age;
-        return true;
-    };
+    friend istream& operator>>(istream& input, Info& people) {
+        cout << "please input your height:";
+        input >> people.height;
+        cout << "please input your weight:";
+        input >> people.weight;
+        cout << "please input your age:";
+        input >> people.age;
+        return input;
+    }
+    friend ostream& operator<<(ostream& output, Info& people) {
+        output << "Hat size = " << people.hat_size() << " in inches." << endl
+               << "Jacket size (chest in inches) = " << people.Jacket_size() << " (chest in inches)" << endl
+               << "Waist = " << people.waist() << " in inches." << endl;
+        return output;
+    }
 
    private:
     double height;
@@ -25,16 +29,10 @@ class info {
 };
 int main() {
     char tmp;
-    while (cout<<"y is continue, q is quit" && cin>>tmp && tmp == 'y') {
-        info people;
-        cout << "please input your height:";
-        people.input_height();
-        cout << "please input your weight:";
-        people.input_weight();
-        cout << "please input your age:";
-        people.input_age();
-        cout << "Hat size = " << people.hat_size() << " in inches." << endl
-             << "Jacket size (chest in inches) = " << people.Jacket_size() << " (chest in inches)" << endl
-             << "Waist = " << people.waist() << " in inches." << endl;
+    while (cout << "y is continue, q is quit" && cin >> tmp && tmp == 'y') {
+        Info people;
+        cin >> people;
+        cout << people;
     }
+    return 0;
 }
