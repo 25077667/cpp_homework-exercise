b746bd1dfac306dd5cbde4f28725342751a95e81
25077667 <zxc25077667@gmail.com>
2019-06-17 06:00:13 +0800

    typesetting code

--- homework/HW1/ch3/p1.cpp
+++ homework/HW1/ch3/p1.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int kilobits_per_megabyte = 8000;
-double video_size(int bit_rate,double video_duration) {
+double video_size(int bit_rate, double video_duration) {
     return video_duration * 60 * bit_rate / kilobits_per_megabyte;
 }
 int main() {
-    double video_duration,bit_rate;  //in minutes
+    double video_duration, bit_rate;  //in minutes
     cout << "Please input the Bit rate" << endl;
-    cin>>bit_rate;
+    cin >> bit_rate;
     cout << "Please input the video duration in minurtes" << endl;
     cin >> video_duration;
-    cout << video_size(bit_rate,video_duration) << " megabytes." << endl;
+    cout << video_size(bit_rate, video_duration) << " megabytes." << endl;
 }
