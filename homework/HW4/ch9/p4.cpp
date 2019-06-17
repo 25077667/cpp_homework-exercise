398a38be7af60a7c213b985dbfbd179d76dfd41e
25077667 <zxc25077667@gmail.com>
2019-06-17 13:37:34 +0800

    TA needs

--- homework/HW4/ch9/p4.cpp
+++ homework/HW4/ch9/p4.cpp
@@ -22,13 +22,10 @@ void operateString(string s, size_t pos = 0) {
                  v.at(iter - v.begin() - 1).back() == '!' ||
                  v.at(iter - v.begin() - 1).back() == '?')
             printHead(((*iter).size() != 4) ? (*iter) : "Love");
-        else if ((*iter).size() == 5 &&
-                 (v.at(iter - v.begin()).back() == '.' ||
-                  v.at(iter - v.begin()).back() == '!' ||
-                  v.at(iter - v.begin()).back() == '?'))
+        else if ((*iter).size() == 5 && ispunct(v.at(iter - v.begin()).back()))
             cout << "love" << v.at(iter - v.begin()).back();
         else if ((*iter).size() == 4 && !ispunct(v.at(iter - v.begin()).back()))
-            cout << "love";  //don't put punct in wht word middle, the question didn't ask to constder it.
+            cout << "love";  //don't put punct in the word middle, the question didn't ask to constder it.
         else
             cout << *iter;
         cout << " ";
