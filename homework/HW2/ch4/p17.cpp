c8436de23984dfaa6903352d5551261a16499d27
25077667 <zxc25077667@gmail.com>
2020-01-13 00:44:51 +0800

    clean code

--- homework/HW2/ch4/p17.cpp
+++ homework/HW2/ch4/p17.cpp
@@ -1,37 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-map<string, int> table;
-void readfile() {
-    fstream file;
-    file.open("scores.txt", ios::in);
+
+void readfile(auto& table) {
+    fstream file("scores.txt", ios::in);
     string name;
     int score;
-    while (file >> name >> score) {
+    while (file >> name >> score)
         table[name] = score;
-    }
     file.close();
 }
-double cal_avg() {
-    int sum=0, counter = 0;
-    map<string, int>::iterator iter;
-    for (iter = table.begin(); iter != table.end(); iter++) {
-        sum += iter->second;
+double cal_avg(auto& table) {
+    int sum = 0, counter = 0;
+    for (auto iter : table) {
+        sum += iter.second;
         counter++;
     }
-    return sum / counter;
+    return sum * 1.0 / counter;
 }
 int main() {
-    readfile();
+    map<string, int> table;
+    readfile(table);
     string name;
-    double average = cal_avg();
+    double average = cal_avg(table);
     cout << "average is " << average << endl;
-    map<string, int>::iterator iter;
     while (cout << "input the name you want " && cin >> name) {
-        iter = table.find(name);
-        if (iter == table.end()) {
+        auto iter = table.find(name);
+        if (iter != table.end())
+            cout << iter->first << " :" << iter->second << " is "
+                 << ((iter->second > average) ? "higher " : "below ") << "than average." << endl;
+        else
             cout << "Not found." << endl;
-            continue;
-        }
-        cout << iter->first << " :" << iter->second << " is " << ((iter->second > average) ? "higher " : "below ") << "than average." << endl;
     }
+    return 0;
 }
