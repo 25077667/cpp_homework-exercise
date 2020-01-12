cde1249ceb13eb11984298c7927a430e38a940ab
25077667 <zxc25077667@gmail.com>
2020-01-13 00:31:58 +0800

    clean code

--- homework/HW2/ch4/p3.cpp
+++ homework/HW2/ch4/p3.cpp
@@ -1,21 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-map<string, int> table;
+
 void calculate(int itemCode, int& voucherValue) {
-    if(itemCode > voucherValue){
+    if (itemCode > voucherValue) {
         cout << "Insufficient balance" << endl;
         return;
     }
     voucherValue -= itemCode;
 }
 int main() {
+    map<string, int> table;
     int voucherValue;
     table["A"] = 10;
     table["B"] = 20;
     table["C"] = 30;
     table["D"] = 40;
-    for (map<string, int>::iterator iter = table.begin(); iter != table.end(); iter++)
-        cout << iter->first << " " << iter->second << endl;
+    for (auto iter : table)
+        cout << iter.first << " " << iter.second << endl;
 
     cout << "Please input your voucher.";
     cin >> voucherValue;
@@ -24,9 +25,11 @@ int main() {
         cout << "Which item do you want? (input the name)";
         cin >> index;
         calculate(table[index], voucherValue);
-        cout << "the price of the item you want to buy " << table[index]<<endl;
-        cout << "now, you have voucher " << voucherValue << endl<<endl;
+        cout << "the price of the item you want to buy " << table[index] << endl;
+        cout << "now, you have voucher " << voucherValue << endl
+             << endl;
     }
     cout << endl
-         << "you bankrupt."<<endl;
+         << "you bankrupt." << endl;
+    return 0;
 }
